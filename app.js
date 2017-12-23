8503e6e21b0692fc5c51278341027cb85c95a0fe
JaeyeonKIM <foodsksms@gmail.com>
2017-12-23 23:29:09 +0900

    Making checking Natural status compare two players

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("gamble project");
+var cards = [
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] ];
+//console.log(cards);
+var player = [4,5];
+var banker = [8,1];
+var sum_player = 0
+var sum_banker = 0
+
+
+var check_the_natural = function (player, banker) {
+  for (var i of player) {
+    sum_player += i;
+    if (sum_player === 8 || sum_player === 9) {
+      console.log("sum of player is Natural");
+    }
+  }
+  for (var j of banker) {
+    sum_banker += j;
+    if (sum_banker === 8 || sum_banker === 9) {
+      console.log("sum of banker is Natural");
+      }
+    }
+  }
+ check_the_natural(player, banker);
